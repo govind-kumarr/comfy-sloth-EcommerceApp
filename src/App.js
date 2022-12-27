5a404c522879d60f9ea9d13dec6a6b39d8423c51
govind kumar <gk4051668@gmail.com>
2022-12-27 17:20:43 +0530

    added lines

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with govind
+          Learn React with govind and shravani
         </a>
       </header>
     </div>
