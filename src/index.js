35ae9aba39a1d7add41b1f4a45ac9d19656325be
Govind Kumar <gk4051668@gmail.com>
2023-03-15 16:38:00 +0530

    added client id and domain

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { ProductsProvider } from './context/products_context'
-import { FilterProvider } from './context/filter_context'
-import { CartProvider } from './context/cart_context'
-import { UserProvider } from './context/user_context'
-import { Auth0Provider } from '@auth0/auth0-react'
-// dev-pu8wyk-g.us.auth0.com
-// 7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    domain="dev-vx0gzof4swjjzes0.us.auth0.com"
+    clientId="wMSKuPCb1fO4OAGSBWwp9BPohNf39yYi"
     redirectUri={window.location.origin}
-    cacheLocation='localstorage'
+    cacheLocation="localstorage"
   >
     <UserProvider>
       <ProductsProvider>
@@ -27,5 +25,5 @@ ReactDOM.render(
     </UserProvider>
   </Auth0Provider>,
 
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
